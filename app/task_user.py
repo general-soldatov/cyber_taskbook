bcefa647c0f16089f3008337c032d397b7256c64
general-soldatov <general-soldatov@mail.ru>
2024-09-08 17:45:03 +0300

    Setting venv and add class solve task

---
+++ app/task_user.py
@@ -0,0 +1,48 @@
+import math
+from random import randint, choice
+
+lst = [1, 2, 3, 4]
+
+class TaskUser:
+    def __init__(self, category: str, level: int):
+        self.category: str = category
+        self.level: int = level
+
+    def __call__(self, number: int) -> dict:
+        if number <= max(lst):
+            text: str = 'Your task miht'
+            image: bool | str = False
+            result: int | float = 3555
+
+            return {
+                'process': 'succesful',
+                'data': {
+                    'text': text,
+                    'image': image,
+                    'result': result
+                }
+            }
+
+        return {
+            'process': 'error',
+            'data': {
+                'text': 'Max number of task'
+                }
+            }
+
+    @staticmethod
+    def task_cod(txt_task):
+        rad = lambda x: x * math.pi / 180
+        grad = lambda x: 180 * x / math.pi
+        lcls = locals()
+        glbl = {
+            'pi': math.pi,
+            'cos': math.cos,
+            'sin': math.sin,
+            'tan': math.tan,
+            'arctan': math.atan,
+            'randint': randint,
+            'choice': choice
+        }
+        exec(txt_task, glbl, lcls)
+        return lcls['text'], lcls['result']
